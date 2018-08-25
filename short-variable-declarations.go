718c28a0cb019e79ed67f25f574629329af67f24
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2018-08-25 23:38:05 +0900

    :memo: challenge tour of go day3

---
+++ short-variable-declarations.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  var i, j int = 1, 2
+  // only use in function
+  k := 3
+  c, python, java := true, false, "no!"
+  fmt.Println(i, j, c, python, java)
+}

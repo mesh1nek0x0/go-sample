b1f6a100543c7ac7513a4990ddb912575321425d
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2018-08-26 23:32:52 +0900

    :memo: challenge tour of go day4

---
+++ type-inference.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  v := 42 // v is an int!
+  hoge := false // hoge is a boolean
+  fmt.Printf("v is type of %T\n", v)
+  fmt.Printf("hoge is type of %T\n", hoge)
+}
